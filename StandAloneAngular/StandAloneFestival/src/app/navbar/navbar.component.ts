b4f24298eaf2b9ed125d815b05ea22455bd27cfb
DwightKSchrute9 <silviolampignano@gmail.com>
2023-09-01 08:32:39 +0200

    modificheNavbar

--- StandAloneAngular/StandAloneFestival/src/app/navbar/navbar.component.ts
+++ StandAloneAngular/StandAloneFestival/src/app/navbar/navbar.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
   isDropdownOpen = false;
+  isNavbarVisible = true;
+  private lastScrollTop = 0;
+
+  toggleDropdown() {
+    if (window.innerWidth <= 767) {
+      // Se la larghezza della finestra è inferiore o uguale a 767px (modalità mobile)
+      this.isDropdownOpen = !this.isDropdownOpen;
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    this.isNavbarVisible = st < this.lastScrollTop;
+    this.lastScrollTop = st;
+  }
 }
