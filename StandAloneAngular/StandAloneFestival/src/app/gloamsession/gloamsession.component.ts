065742dfad9eef9f304b09802bf5e65b3ba6ed05
DwightKSchrute9 <silviolampignano@gmail.com>
2023-09-05 09:00:11 +0200

    playlist_Accordion_GloamSessionComponent

--- StandAloneAngular/StandAloneFestival/src/app/gloamsession/gloamsession.component.ts
+++ StandAloneAngular/StandAloneFestival/src/app/gloamsession/gloamsession.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class GloamsessionComponent {
 
+  isSection1Open = false;
+  isSection2Open = false;
+  isSection3Open = false;
+  isSection4Open = false;
+  isSection5Open = false;
+
+  // Funzione per gestire l'apertura e la chiusura di un accordion
+  toggleAccordion(section: number): void {
+    switch (section) {
+      case 1:
+        this.isSection1Open = !this.isSection1Open;
+        break;
+      case 2:
+        this.isSection2Open = !this.isSection2Open;
+        break;
+      case 3:
+        this.isSection3Open = !this.isSection3Open;
+        break;
+      case 4:
+        this.isSection3Open = !this.isSection4Open;
+        break;
+      case 5:
+        this.isSection3Open = !this.isSection5Open;
+        break;
+      // Aggiungi altri casi per ulteriori sezioni
+    }
+  }
+
 }
